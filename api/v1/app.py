85b010b96980526c4c84df9aeee64c76d01bcdc5
Samuel Mukosa Chibwe <102081659+chibwesamuel@users.noreply.github.com>
2023-05-01 13:03:52 +0200

    Added error handling with 404 response

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,11 @@ def teardown_storage(exception):
     """Calls storage.close()"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ = __main__:
     import os
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
