563d9d06c80226d45b5b6aefb511b1b67b1047bc
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-01 19:29:47 +0100

    api fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ Registers the blueprint and runs the Flask server
 """
 from flask import Flask, jsonify
 from api.v1.views import app_views
-from models import FileStorage
+from models import storage
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-@app.teardown_appcontent
+
+@app.teardown_appcontext
 def teardown_storage(exception):
     """Calls storage.close()"""
     storage.close()
@@ -21,7 +23,6 @@ def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-    import os
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
