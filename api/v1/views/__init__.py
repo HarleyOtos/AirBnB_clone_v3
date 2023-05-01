4242daf94478519a29303a439832593f8fdd859a
Samuel Mukosa Chibwe <102081659+chibwesamuel@users.noreply.github.com>
2023-05-01 08:54:56 +0200

    Documentation fixed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#Imports of everything in the package api.v1.views.index
+"""
+Imports of everything in the package api.v1.views.index
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
