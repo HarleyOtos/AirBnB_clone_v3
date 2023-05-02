cfe76cd44ec178ce3e1d58d45a4a4540ba07580e
Samuel Mukosa Chibwe <102081659+chibwesamuel@users.noreply.github.com>
2023-05-02 19:25:12 +0200

    App.py update

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,26 @@ def not_found(error):
     """Handles 404 errors"""
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """Handles Global errors"""
+    if isinstance(err, HTTPException):
+        if (isinstance(err).__name__ == 'NotFound'):
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
+
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
